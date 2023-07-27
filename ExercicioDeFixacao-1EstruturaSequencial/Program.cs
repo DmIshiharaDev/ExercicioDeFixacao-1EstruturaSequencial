053ff96cbc85e6b266b52bfbade5fb15a6c5582d
DmIshiharaDev <139144547+DmIshiharaDev@users.noreply.github.com>
2023-07-27 06:26:52 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioDeFixacao-1EstruturaSequencial/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Por favor, entre com o primeiro valor inteiro: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Por favor, entre com o segundo número inteiro: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        int soma = num1 + num2;
+
+        Console.WriteLine("A soma desses valores é: " + soma);
+    }
+}
